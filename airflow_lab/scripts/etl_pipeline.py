81f3c02008d3c3be9b49ac44bb731e38a4a897fd
Raphael Pereira <raphatpereira@gmail.com>
2026-04-08 21:39:33 +0000

    adiciona validacao de qualidade de dados

--- airflow_lab/scripts/etl_pipeline.py
+++ airflow_lab/scripts/etl_pipeline.py
@@ -8,6 +8,22 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+def validate_data(df):
+
+    logging.info("validando qualidade dos dados")
+
+    if df.empty:
+        raise ValueError("dataset vazio")
+
+    if df["valor"].isnull().any():
+        raise ValueError("coluna valor contém nulos")
+
+    if (df["valor"] < 0).any():
+        raise ValueError("valores negativos encontrados")
+
+    logging.info("dados validados com sucesso")
+
+
 def run_pipeline():
 
     file_path = "/opt/airflow/data/big_dataset.csv"
@@ -20,6 +36,8 @@ def run_pipeline():
     df = pd.read_csv(file_path)
 
     logging.info(f"{len(df)} registros encontrados")
+
+    validate_data(df)
 
     logging.info("iniciando transformação")
 
